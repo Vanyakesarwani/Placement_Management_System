7d0a3f360ba45effad051e4b9318335e2eedff70
Vanyakesarwani <kesharwanivanya@gmail.com>
2026-08-30 22:49:52 +0530

    Add Student Registration

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =( 'mysql+pymysql://root:vanya7890@localhost/placement_db'
-                                        '?unix_socket=/tmp/mysql.sock')
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'mysql+pymysql://root@localhost/placement_db'
+    '?unix_socket=/tmp/mysql.sock'
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,14 +19,40 @@ class Student(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     branch = db.Column(db.String(50))
 
+
 @app.route("/")
 def home():
     return "Placement Management System Backend is Running!"
-    return "Database connection successful!"
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    # When user opens /register
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # When user submits the form
+    name = request.form["name"]
+    email = request.form["email"]
+    branch = request.form["branch"]
+
+    # Create student object
+    student = Student(
+        name=name,
+        email=email,
+        branch=branch
+    )
+
+    # Insert into database
+    db.session.add(student)
+    db.session.commit()
+
+    return "Registration successful!"
+
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # Create tables if they don't exist
-
+        db.create_all()
 
     app.run(debug=True)
